c591d1ba10f7a62a6bab3193d2da3fd56ca2cb93
Bruno Marques <brunomn95@gmail.com>
2018-11-26 14:44:37 -0200

    Updates

---
+++ precache-manifest.b89665aba3a9170eeae026e6d7d192be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57b5ceb29dc67fdac2f4",
+    "url": "/i18n-ufsc/static/js/runtime~main.57b5ceb2.js"
+  },
+  {
+    "revision": "5508abea19b64e1325ab",
+    "url": "/i18n-ufsc/static/js/main.5508abea.chunk.js"
+  },
+  {
+    "revision": "b3ea2268ec9b98b1eb4c",
+    "url": "/i18n-ufsc/static/js/1.b3ea2268.chunk.js"
+  },
+  {
+    "revision": "5508abea19b64e1325ab",
+    "url": "/i18n-ufsc/static/css/main.da5f73a5.chunk.css"
+  },
+  {
+    "revision": "62bbd6490a1b40fff4088e9db1f2ad77",
+    "url": "/i18n-ufsc/index.html"
+  }
+];
